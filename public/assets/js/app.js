3f3760dd2a6a58e26dfa5987af19b6e5034391ff
Michael Johnson <michaeljohnson@Michaels-MacBook-Pro.local>
2018-05-08 10:02:28 -0700

    fixed notes

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -5,9 +5,7 @@ $("#scrape-button").on("click", function () {
         location.reload();
     })
 })
-
-$("body").on("click", "#note-button", function () {
-    const thisId = $(this).attr("data-id");
+const getData = function(thisId){
     $("#note-title").val("");
     $("#note-body").val("");
     $.ajax({
@@ -25,27 +23,35 @@ $("body").on("click", "#note-button", function () {
             $("#note-body").val("");
         }
     });
+}
+$("body").on("click", "#note-button", function () {
+    const thisId = $(this).attr("data-id");
+    getData(thisId);
+    $("#submit").attr("data-id", "");
+    $("#submit").attr("data-id", thisId);
     $(".modal").css("display", "block");
-    $("#new-note").on("submit", function (e) {
-        e.preventDefault();
-        const title = $("#note-title").val().trim();
-        const note = $("#note-body").val().trim();
-        const noteRoute = `/notes/${thisId}`;
-        $.ajax({
-            method: "POST",
-            url: noteRoute,
-            data: {
-                title: title,
-                body: note
-            }
-        });
-    });
-    const span = document.getElementsByClassName("close")[0];
+    
+});
+const span = document.getElementsByClassName("close")[0];
     span.onclick = function () {
         $(".modal").css("display", "none");
     };
-});
 
+$("body").on("click", "#submit", function (e) {
+    e.preventDefault();
+    const title = $("#note-title").val().trim();
+    const note = $("#note-body").val().trim();
+    const btnId = $("#submit").attr("data-id");
+    const noteRoute = `/notes/${btnId}`;
+    $.ajax({
+        method: "POST",
+        url: noteRoute,
+        data: {
+            title: title,
+            body: note
+        }
+    });
+});
 $("body").on("click", "#delete-all-button", function () {
     const deleteRoute = "/articles/removeall"
     $.post(deleteRoute, function (data) {
