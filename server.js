0b1ea2971c8abcb3fe687724c4e74eb1001e48b8
Michael Johnson <michaeljohnson@Michaels-MacBook-Pro.local>
2018-05-07 17:58:50 -0700

    cleared some comments

--- server.js
+++ server.js
@@ -38,38 +38,27 @@ app.get("/", function (req, res) {
 
 app.post("/scraper", function (req, res) {
 
-    // First, we grab the body of the html with request
     axios.get("https://www.nytimes.com/").then(function (response) {
-        // Then, we load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(response.data);
 
-        // Now, we grab every h2 within an article tag, and do the following:
         $("h2.story-heading").each(function (i, element) {
-            // Save an empty result object
             var result = {};
-
-            // Add the text and href of every link, and save them as properties of the result object
             result.title = $(this)
                 .children("a")
                 .text();
             result.link = $(this)
                 .children("a")
-                .attr("href");
-
+                 .attr("href");
 
             console.log(result);
-            // Create a new Article using the `result` object built from scraping
             db.Article.create(result)
                 .then(function (dbArticle) {
-                    // View the added result in the console
                     console.log(dbArticle);
                 })
                 .catch(function (err) {
-                    // If an error occurred, send it to the client
                     return res.json(err);
                 });
         })
-        // If we were able to successfully scrape and save an Article, send a message to the client
         res.refresh();
     });
 });
